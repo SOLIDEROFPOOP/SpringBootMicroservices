cd3fe1180b03dad107297d8740f88a82a0d359d1
Kapparov Murat <paprim2003@gmail.com>
2024-06-15 21:08:20 +0500

    all services moved under maven multi module

---
+++ microservice/inventory-service/src/main/java/com/example/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,21 @@
+package com.example.inventoryservice.controller;
+
+import com.example.inventoryservice.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.RequestScope;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+
+    @Autowired
+    private InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+        return inventoryService.isInStock(skuCode);
+    }
+}
